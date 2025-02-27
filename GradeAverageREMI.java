ce16d95a5bd683e1265855ec65445a59a798679e
Remi <zestysourcitrus@gmail.com>
2025-02-27 09:58:31 -0500

    kkuyu7uu

---
+++ GradeAverageREMI.java
@@ -0,0 +1,23 @@
+//Remiiiiiiiiiiiiiiiiiiiiiiiiii-Laurent Guy
+
+import java.util.*;
+
+public class GradeAverageREMI {
+    public static void main(String[]args){
+
+
+
+
+int grade;
+
+Scanner input = new Scanner(System.in);
+//You gotta prompt the user for the grades I guess
+System.out.println("Enter grade pls");
+
+grade = input.nextInt();
+
+System.out.println("your got a " + grade + "? that's pretty bad ngl");
+
+
+    }
+}
