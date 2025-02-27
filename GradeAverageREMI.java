f2aa69f052e35b04fa11ba0d45913efe88b5136c
Remi <zestysourcitrus@gmail.com>
2025-02-27 10:09:11 -0500

    btrhth

--- GradeAverageREMI.java
+++ GradeAverageREMI.java
@@ -9,6 +9,12 @@ public class GradeAverageREMI {
 
 
 int grade;
+int grade2;
+int grade3;
+int grade4;
+int grade5;
+double average;
+
 
 Scanner input = new Scanner(System.in);
 //You gotta prompt the user for the grades I guess
@@ -18,6 +24,31 @@ grade = input.nextInt();
 
 System.out.println("your got a " + grade + "? that's pretty bad ngl");
 
+System.out.println("Enter your friend's grade pls");
 
+grade2 = input.nextInt();
+
+System.out.println("your friend got a " + grade2 + "? you gotta get to your friend's level man");
+
+System.out.println("Enter another friend's grade pls");
+
+grade3 = input.nextInt();
+
+System.out.println("your other friend got a " + grade3 + "? you gotta get to your other friend's level man");
+
+System.out.println("Enter another friend's grade pls");
+
+grade4 = input.nextInt();
+
+System.out.println("your other friend got a " + grade4 + "? you gotta get to your other friend's level man");
+
+System.out.println("Enter another friend's grade pls");
+
+grade5 = input.nextInt();
+
+System.out.println("your other friend got a " + grade5 + "? you gotta get to your other friend's level man");
+
+average = (grade + grade2 + grade3 + grade4 + grade5)/5;
+System.out.println("The average of all of your awful grades is " + average);
     }
 }
