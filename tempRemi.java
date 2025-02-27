1fbcb97ed881e4e2ed1f249c51ac90866d079d76
Remi <zestysourcitrus@gmail.com>
2025-02-27 10:30:15 -0500

    hyhyujyj

---
+++ tempRemi.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class tempRemi {
+    public static void main(String[]args){
+
+double temp;
+double celc;
+
+Scanner input = new Scanner(System.in);
+System.out.println("What's the temp in Fahrenheit?");
+temp = input.nextInt();
+celc = (5/9*(temp-32));
+System.out.println("That temperature in Celsius is " + celc);
+
+
+
+
+
+
+    }
+
+
+}
