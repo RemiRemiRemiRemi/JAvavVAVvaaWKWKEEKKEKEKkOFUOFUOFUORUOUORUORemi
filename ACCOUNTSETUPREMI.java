60e94c4bb32066789f719c7bae98921234d8b0e7
Remi <zestysourcitrus@gmail.com>
2025-03-03 10:29:59 -0500

    grgrgr

---
+++ ACCOUNTSETUPREMI.java
@@ -0,0 +1,32 @@
+//C'est moi, Remi!
+//Remi-Laurent Guy, 2/25/25
+
+import java.util.*;
+
+public class ACCOUNTSETUPREMI{
+    public static void main(String[]args){
+
+
+
+
+String first;
+String last;
+String pass;
+Scanner input = new Scanner(System.in);
+
+System.out.println("What's your first name?????????");
+
+first = input.next();
+
+System.out.println("What's your lastttttttttttt nameeeeeeee???????????????????????");
+
+last = input.next();
+
+System.out.println("Ok, hi " + first + " " + last + ", now what's your password?");
+
+pass = input.next();
+
+System.out.println(pass + "? that's a nice password. Thanks for your personal info >:)");
+
+    }
+}
